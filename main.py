39fa5850324a379e5366796dd5d70e479c277e1a
Daniel Gallardo Romero <daniel.s265114@cesurformacion.com>
2021-12-17 18:55:08 +0100

    modifican lineas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
     print("mi primera linea de codigo")
-    print("mi primera linea de codigo")
-    print("mi primera linea de codigo")
+    print("mi segunda linea de codigo")
+    print("mi tercera linea de codigo")
